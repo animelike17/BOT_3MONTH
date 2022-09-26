db7a5e25de6acd97c2003178adbf6f4486debc44
animelike17 <110774126+animelike17@users.noreply.github.com>
2022-09-26 12:00:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ from aiogram.utils import executor
 from config import dp, bot
 import logging
 import asyncio
-
 from handlers import client, extra, callback, admin, fsmAdminMenu
 from database.bot_dp import sql_creat
 
 
 async def on_startup(_):
-    sql_create()
+     asyncio.create_task(notifications.scheduler())
 
 
 client.register_handlers_client(dp)
 fsmAdminMenu.register_handlers_FSMAdmin(dp)
+notifications.register_handlers_notifications(dp)
 callback.register_callback_handlers(dp)
-# admin.register_handlers_admin(dp)
 extra.register_extra_handlers(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
     
